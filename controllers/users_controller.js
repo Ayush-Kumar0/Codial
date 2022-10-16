9001c47daa5437680f828a2e234c22547f4510c4
Ayush-Kumar0 <ayushkumar27053@gmail.com>
2022-10-16 11:59:46 +0530

    Added layout functionality

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,15 @@
-module.exports.profile = function (req, res) { 
+module.exports.profile = function (req, res) {
     res.end('This is the profile page');
-};
+}
+
+module.exports.signUp = function (req, res) {
+    return res.render('user_sign_up', {
+        title: 'User | Sign Up'
+    });
+}
+
+module.exports.signIn = function (req, res) {
+    return res.render('user_sign_in', {
+        title: 'User | Sign In'
+    });
+}
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const express=require('express');
-const app=express();
-const port=8000;
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const port = 8000;
 
-//Use express.Router middleware
-app.use('/', require('./routes'));
+//Static files
+app.use(express.static('./assets'));
+
+//Setting up views
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+//Setting up layouts
+app.use(expressLayouts);
+app.set('layout', './layout');
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
-app.listen(port,'localhost',function (err) {
-    if(err){
+//Use express.Router middleware
+app.use('/', require('./routes'));
+
+app.listen(port, 'localhost', function (err) {
+    if (err) {
         console.log(`Error while connecting to server on port : ${err}`);
         return;
     }
